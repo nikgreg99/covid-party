e4cdef86642d89de5f7cacbd01fcc5beef82e164
DESKTOP-P2OC9CF\User <fingold@rocketmail.com>
2021-10-20 17:53:44 +0200

    terrain base generation and lilman animation

---
+++ Assets/Scripts/Environment/TerrainGenerator.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    //public delegate void GenerationAction();
+    //public static event GenerationAction generated;
+
+    private Terrain _terrain;
+
+    [SerializeField] private int _width;
+    [SerializeField] private int _depth;
+    [SerializeField] private int _height;
+
+    [SerializeField] private int _scale;
+
+    [SerializeField] private int _originX;
+    [SerializeField] private int _originY;
+    [SerializeField] private bool _randomGen = true;
+
+    [SerializeField] private Transform _playerSpawnTransform;
+    [SerializeField] private float _playerSapwnYOffset;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _terrain = GetComponent<Terrain>();
+        this.transform.position = new Vector3(-_width / 2, 0, -_height / 2);
+        _terrain.terrainData = GenerateTerrainData(_terrain.terrainData);
+
+        SpawnGivenPlayer();
+        SpwanWalls();
+    }
+
+    private void SpwanWalls()
+    {
+        GameObject wallParent = GameObject.FindGameObjectWithTag("WallParent");
+
+        GameObject wall1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall1.transform.position = new Vector3(_width / 2, _depth, 0);
+        wall1.transform.localScale = new Vector3(1, _depth * 2, _height);
+        wall1.transform.SetParent(wallParent.transform);
+
+        GameObject wall2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall2.transform.position = new Vector3(-_width / 2, _depth, 0);
+        wall2.transform.localScale = new Vector3(1, _depth * 2, _height);
+        wall2.transform.SetParent(wallParent.transform);
+
+        GameObject wall3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall3.transform.position = new Vector3(0, _depth, _height / 2);
+        wall3.transform.localScale = new Vector3(_width, _depth * 2, 1);
+        wall3.transform.SetParent(wallParent.transform);
+
+        GameObject wall4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall4.transform.position = new Vector3(0, _depth, -_height / 2);
+        wall4.transform.localScale = new Vector3(_width, _depth * 2, 1);
+        wall4.transform.SetParent(wallParent.transform);
+    }
+
+    private void SpawnGivenPlayer()
+    {
+        if (_playerSpawnTransform != null)
+        {
+            _playerSpawnTransform.position = new Vector3(0, _terrain.terrainData.GetInterpolatedHeight(0, 0) + _playerSapwnYOffset, 0);
+        }
+    }
+
+    private TerrainData GenerateTerrainData(TerrainData terrainData)
+    {
+        terrainData.size = new Vector3(_width, _depth, _height);
+        terrainData.heightmapResolution = _width + 1;
+
+        float[,] heights = GenerateHeights();
+
+        terrainData.SetHeights(0, 0, heights);
+
+        return terrainData;
+    }
+
+    private float[,] GenerateHeights()
+    {
+        float offsX = _randomGen ? Random.Range(0, 400) : _originX;
+        float offsY = _randomGen ? Random.Range(0, 400) : _originY;
+
+        float[,] heights = new float[_width, _height];
+
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+
+                float perlinX = (float)x / _width * _scale + offsX;
+                float perlinY = (float)y / _height * _scale + offsY;
+
+                heights[x, y] = Mathf.PerlinNoise(perlinX, perlinY);
+            }
+
+        }
+
+        return heights;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DoGenerate();
+    }
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,11 +18,14 @@ public class PlayerMovement : MonoBehaviour
     private bool _mouseMode = false;
     private bool _speedUp = false;
 
+    private List<Animator> _animators = new List<Animator>();
+    private bool _lastMoving = false;
+
     private void UpdateTranslation(float moveX, float moveZ)
     {
         _translation = Vector3.zero;
-        _translation += Vector3.right * moveX;
-        _translation += Vector3.forward * moveZ;
+        _translation += transform.right * moveX;
+        _translation += transform.forward * moveZ;
 
         if (_speedUp)
         {
@@ -52,11 +55,11 @@ public class PlayerMovement : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                rotationY -= 1;
+                rotationY = -1;
             }
             else if (Input.GetKeyDown(KeyCode.E))
             {
-                rotationY += 1;
+                rotationY = 1;
             }
         }
         _rotation = Vector3.up * rotationY;
@@ -67,11 +70,15 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        foreach (Animator a in GetComponentsInChildren<Animator>())
+        {
+            _animators.Add(a);
+        }
     }
 
     void Update()
@@ -79,8 +86,23 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
+        bool moving = moveX != 0 || moveZ != 0;
+
+        if (_lastMoving != moving)
+        {
+            _lastMoving = moving;
+
+            foreach (Animator a in _animators)
+            {
+                a.SetBool("moving", moving);
+            }
+        }
+
+
+
+
         _speedUp = Input.GetKeyDown(KeyCode.LeftShift);
-            
+
         UpdateTranslation(moveX, moveZ);
         UpdateRotation();
     }
@@ -91,7 +113,7 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(_rotation * _speedRotate * Time.fixedDeltaTime));
     }
 }
- 
-   
 
-   
+
+
+
