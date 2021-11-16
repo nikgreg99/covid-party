8fa450cee8e0f8d70e30ea79ece28773fb37f8d2
Nicolas Gregori <nicolas.gregori@student.supsi.ch>
2021-11-16 21:14:11 +0100

    Fix cameras

--- Assets/Scripts/Camera/FirstPersonCamera.cs
+++ Assets/Scripts/Camera/FirstPersonCamera.cs
@@ -4,39 +4,29 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
-    [SerializeField] private float _rotXSpeed = 1.5f;
-    [SerializeField] private float _rotYSpeed = 1.5f;
-    [SerializeField] private float _rotYMaxAngle = 360.0f;
 
-    private float _rotX;
-    private float _rotY;
+    [SerializeField] private float _sensitivity = 100.0f;
+    [SerializeField] private float _maxAngleRotationX = 45.0f;
+    [SerializeField] private Transform _player;
+
+    private float _rotX = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rotX = _target.eulerAngles.x;
-        _rotY = _target.eulerAngles.y;
-    }
-
-    public void updateRotationAxes(float x, float y)
-    {
-        _rotX = x;
-        _rotY = y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_rotX);
-        Debug.Log(_rotY);
-        _rotX += Input.GetAxis("Mouse X") * _rotXSpeed;
-        _rotY += Input.GetAxis("Mouse Y") * _rotYSpeed;
+        float mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
 
-        _rotY = Mathf.Clamp(_rotY ,-_rotYMaxAngle, _rotYMaxAngle);
+        _rotX -= mouseY;
+        _rotX = Mathf.Clamp(_rotX, -_maxAngleRotationX, _maxAngleRotationX);
 
-        Quaternion xQuat = Quaternion.AngleAxis(_rotX, Vector3.up);
-        Quaternion yQuat = Quaternion.AngleAxis(_rotY, Vector3.left);
-        _target.rotation = xQuat * yQuat;
+        transform.localRotation = Quaternion.Euler(_rotX, 0.0f, 0.0f);
+        _player.Rotate(Vector3.up * mouseX);
+
     }
 }
