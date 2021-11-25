61e582bb4fd9286987387b1dd10dbba1bb783747
DESKTOP-9KL33H9\Manel <manel.gf.95@gmail.com>
2021-11-25 20:38:23 +0100

    navmesh first try outs

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -1,83 +1,35 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIMovement : MonoBehaviour
 {
+    [SerializeField] private Transform target;
 
-    Rigidbody body;
-    GameObject player;
+    private Rigidbody body;
+    private NavMeshAgent agent;
 
-    Vector3 moveDir;
-    public int speed = 1;
-    int initialSpeed = 0;
-
-    public float maxDistFromObstacle = 0.0f;
-    public float maxDistFromPlayer = 0.0f;
-    float counter = 0.0f;
-
-    public LayerMask groundMask;
-    public LayerMask obstacleMask;
-
-    private List<Animator> _animators = new List<Animator>();
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        body = gameObject.GetComponent<Rigidbody>();
-        moveDir = chooseDirection();
-        transform.rotation = Quaternion.LookRotation(moveDir);
-        player = GameObject.FindGameObjectWithTag("Player");
-        initialSpeed = speed;
+        body = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+    }
 
-        foreach (Animator a in GetComponentsInChildren<Animator>())
+    private void Update()
+    {
+        if (agent.enabled)
         {
-            _animators.Add(a);
+            agent.destination = target.position;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        foreach (Animator a in _animators)
+        if(collision.gameObject.layer == 7)
         {
-            a.SetBool("moving", true);
+            agent.enabled = true;
+            body.isKinematic = true;
+            Debug.Log("Touched ground");
         }
-
-        body.MovePosition(body.position + transform.forward * Time.deltaTime * speed);
-
-        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-
-        counter += Time.deltaTime;
-
-        if(Physics.CheckSphere(transform.position, maxDistFromObstacle, obstacleMask) && (distanceToPlayer < maxDistFromPlayer))
-        {
-            speed = 0;
-        }
-        else if ((Physics.CheckSphere(transform.position, maxDistFromObstacle, obstacleMask) || counter > Random.Range(10f, 15f)) && (distanceToPlayer > maxDistFromPlayer))
-        {
-            speed = initialSpeed;
-            moveDir = chooseDirection();
-            transform.rotation = Quaternion.LookRotation(moveDir);
-            counter = 0.0f;
-        }
-        else if (distanceToPlayer < 5f)
-        {
-            speed = 0;
-        }
-        else if (distanceToPlayer < maxDistFromPlayer)
-        {
-            speed = initialSpeed;
-            Vector3 playerPos = new Vector3(player.transform.position.x, 0, player.transform.position.z);
-            Vector3 enemyPos = new Vector3(transform.position.x, 0, transform.position.z);
-            moveDir = (enemyPos - playerPos).normalized;
-            transform.rotation = Quaternion.LookRotation(moveDir);
-        }
-    }
-
-    Vector3 chooseDirection()
-    {
-        Vector3 dir = Random.onUnitSphere;
-        dir.y = 0;
-        return dir.normalized;
     }
 }
