28ccb32635ae72cc3a87a779e3e0b2e8766f7089
Manuel Grgic Figueiredo <manuel.grgic@student.supsi.ch>
2021-12-03 11:50:29 +0100

    Enemies run away

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -9,12 +9,13 @@ public class AIMovement : MonoBehaviour
     [SerializeField] private float wanderRadius = 1000f;
     [SerializeField] private float wanderTime = 10f;
     [SerializeField] private float minDistanceToPlayer = 10f;
-    [SerializeField] private float multiplyBy = 3f;
+    [SerializeField] private float multiplyBy = 10f;
 
     private Transform target;
     private List<Animator> _animators = new List<Animator>();
     private float timer;
     private float speed;
+    private float angularSpeed;
 
     private void Start()
     {
@@ -23,11 +24,18 @@ public class AIMovement : MonoBehaviour
         {
             _animators.Add(a);
         }
+
+        transform.position = new Vector3(transform.position.x, 20, transform.position.z);
+
         agent.enabled = false;
+        body.isKinematic = false;
+        body.useGravity = true;
+
         agent.baseOffset = 2;
         agent.height = 0.5f;
         timer = wanderTime;
         speed = agent.speed;
+        angularSpeed = agent.angularSpeed;
     }
 
     private void Update()
@@ -37,25 +45,28 @@ public class AIMovement : MonoBehaviour
         {
             timer += Time.deltaTime;
 
-            if (timer >= wanderTime)
+            float distance = Vector3.Distance(target.position, transform.position);
+
+            if (distance < minDistanceToPlayer)
+            {
+                Vector3 newPos = transform.position + ((transform.position - target.position).normalized * multiplyBy);
+                agent.speed = 2 * speed;
+                agent.angularSpeed = 100 * angularSpeed;
+                agent.SetDestination(newPos);
+            }
+
+            if (timer >= wanderTime && (distance > minDistanceToPlayer))
             {
                 foreach (Animator a in _animators)
                 {
                     a.SetBool("moving", true);
                 }
 
-                if (Vector3.Distance(target.position, transform.position) < minDistanceToPlayer)
-                {
-                    Vector3 newPos = (transform.position - target.position).normalized * multiplyBy;
-                    agent.speed = 2 * speed;
-                    agent.SetDestination(newPos);
-                }
-                else
-                {
-                    Vector3 newPos = RandomMove(transform.position, wanderRadius, -1);
-                    agent.SetDestination(newPos);
-                    agent.speed = speed;
-                }
+                Vector3 newPos = RandomMove(transform.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                agent.speed = speed;
+                agent.angularSpeed = angularSpeed;
+                
                 timer = 0;
             }
         }
