41e47b9ce77c5df6793259d71833294b5ed35681
DESKTOP-P2OC9CF\User <fingold@rocketmail.com>
2022-01-30 16:45:18 +0100

    tiny fix tutorial

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
     private static AudioManager _instance;
